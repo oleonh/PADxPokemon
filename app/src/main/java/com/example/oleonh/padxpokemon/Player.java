ba2315d2a5d9019d8e50026f3ca59d8c0e3ebaaa
Ameya Mandalik <amandalik.loyola@gmail.com>
2018-03-17 13:48:34 -0700

    yolo

--- app/src/main/java/com/example/oleonh/padxpokemon/Player.java
+++ app/src/main/java/com/example/oleonh/padxpokemon/Player.java
@@ -9,14 +9,24 @@ import java.util.ArrayList;
 public class Player {
     String name;
     ArrayList<Pokemon> pokemen = new ArrayList<Pokemon>();
-
+    ArrayList<Pokemon> inventory = new ArrayList<>();
 
     //boolean reveals if the pokemon can be added
-    boolean addPokemon(Pokemon p) {
-        if (pokemen.size() >= 5) {
-            return false;
+    void addPokemon(Pokemon p) {
+        if (pokemen.size() > 6) {
+            inventory.add(p);
+            System.out.println("to inventory");
         }
         pokemen.add(p);
-        return true;
+        System.out.println("done");
+    }
+
+
+    //i is index of pokemon from inventory
+    //j is index of pokemon from bag
+    void swapFromInventorytoHand(int i, int j){
+        Pokemon p = pokemen.get(i);
+        pokemen.set(i, inventory.set(j, p));
+        System.out.println("done");
     }
 }
