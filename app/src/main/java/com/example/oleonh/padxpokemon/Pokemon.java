c6c594f477a6d6e58eb02ac72d303b0f739b49dc
oleonh <oleon.bball@gmail.com>
2018-03-17 11:58:26 -0700

    more edits to pokemon class

--- app/src/main/java/com/example/oleonh/padxpokemon/Pokemon.java
+++ app/src/main/java/com/example/oleonh/padxpokemon/Pokemon.java
@@ -1,3 +1,5 @@
+package com.example.oleonh.padxpokemon;
+
 /**
  * Created by oleonh on 3/17/18.
  */
@@ -7,4 +9,15 @@ public class Pokemon {
     int defense;
     int agility;
     String type;
+
+    public Pokemon(){
+
+    }
+
+    public Pokemon(int att, int def, int agi, String name){
+        attack = att;
+        defense = def;
+        agility = agi;
+        type = name; 
+    }
 }
