5bd329578b229510c60675551f0c3cc2ef6b441c
oleonh <oleon.bball@gmail.com>
2018-03-17 13:18:51 -0700

    small edits to the GameGrid class

--- app/src/main/java/GameGrid.java
+++ app/src/main/java/GameGrid.java
@@ -5,6 +5,10 @@
 public class GameGrid {
     int[][] ground;
     boolean[][] playerlocation;
+    final private int ROCK = 2;
+    final private int GRASS = 1;
+    final private int GROUND = 0;
+
 
     public GameGrid(){
         int[][] ground = new int[20][20];
@@ -12,12 +16,12 @@ public class GameGrid {
            for (int j = 0; j < 20; j++){
                  if (i < 7){
                      if(j == 0 || j == 19) {
-                         ground[i][j] = 2;
+                         ground[i][j] = ROCK;
                      }else{
-                         ground[i][j] = 1;
+                         ground[i][j] = GRASS;
                      }
                  }else{
-                     ground[i][j] = 0;
+                     ground[i][j] = GROUND;
                  }
             }
         }
