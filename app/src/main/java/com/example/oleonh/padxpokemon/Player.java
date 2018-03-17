bfe7407fbec5d8f8a191c5c184b9e8925374fad4
Ameya Mandalik <amandalik.loyola@gmail.com>
2018-03-17 13:28:05 -0700

    Oops

--- app/src/main/java/com/example/oleonh/padxpokemon/Player.java
+++ app/src/main/java/com/example/oleonh/padxpokemon/Player.java
@@ -4,14 +4,16 @@ package com.example.oleonh.padxpokemon;
  * Created by kamehameya on 3/17/18.
  */
 
+import java.util.ArrayList;
+
 public class Player {
     String name;
-    Araylist<Pokemon> pokemen = new Arraylist<>();
+    ArrayList<Pokemon> pokemen = new ArrayList<Pokemon>();
 
 
     //boolean reveals if the pokemon can be added
     boolean addPokemon(Pokemon p) {
-        if (pokemen.getSize() >= 5) {
+        if (pokemen.size() >= 5) {
             return false;
         }
         pokemen.add(p);
