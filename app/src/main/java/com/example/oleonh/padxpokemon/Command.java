ffe982e0ea8c4058fc7d2d608a8a772703e34211
Ameya Mandalik <amandalik.loyola@gmail.com>
2018-03-17 15:49:52 -0700

    check in

---
+++ app/src/main/java/com/example/oleonh/padxpokemon/Command.java
@@ -0,0 +1,9 @@
+package com.example.oleonh.padxpokemon;
+
+/**
+ * Created by kamehameya on 3/17/18.
+ */
+
+public class Command {
+
+}
--- app/src/main/java/com/example/oleonh/padxpokemon/GameGrid.java
+++ app/src/main/java/com/example/oleonh/padxpokemon/GameGrid.java
@@ -4,6 +4,8 @@ package com.example.oleonh.padxpokemon;
  * Created by oleonh on 3/17/18.
  */
 
+import java.util.*;
+
 public class GameGrid {
     int[][] ground;
     boolean[][] playerlocation;
@@ -12,7 +14,7 @@ public class GameGrid {
     final private int GROUND = 0;
     int playerX;
     int playerY;
-
+    ArrayList<Pokemon> allPokemon = new ArrayList<>();
 
     public GameGrid(){
         int[][] ground = new int[20][20];
@@ -29,12 +31,33 @@ public class GameGrid {
                  }
             }
         }
+        allPokemon.add(new Pokemon(10, 10, 10, "dark", "OG"));
         playerX = 10;
         playerY = 10;
     }
 
+    void runIntoPokemon(){
+        if(isInGrass()){
+            if(Math.random() <= .25){
+                fightRandomPokemon();
+            }
+        }
+    }
+
+    void fightRandomPokemon(){
+
+    }
+
+
     void playerMove(int x1, int y1){
         playerX = x1; playerY = y1;
+    }
+
+    boolean isInGrass(){
+        if(ground[playerX][playerY] == GRASS){
+            return true;
+        }
+        return false;
     }
 
 }
