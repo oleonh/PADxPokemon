a5264ba0ed25eb6ad4afd3632a7c85b2990214fa
vgmachinist <37482860+vgmachinist@users.noreply.github.com>
2018-03-17 16:03:32 -0700

    made the world class so that it actually works lmao

--- app/src/main/java/com/example/oleonh/padxpokemon/World.java
+++ app/src/main/java/com/example/oleonh/padxpokemon/World.java
@@ -5,31 +5,36 @@ package com.example.oleonh.padxpokemon;
  */
 
 import android.graphics.*;
-<<<<<<< HEAD
-
-public class World {
-    public World(){
-        for(int i = 0; i < 20; i++){
-            for(int j = 0; j < 20; j++){
-                if(GameGrid.getArrayValue(i, j) == 2){
-
-                }else if(GameGrid.getArrayValue(i, j) == 1){
-
-                }else{
-=======
 import android.media.Image;
 import android.widget.ImageView;
 
 public class World {
+
     private final int WIDTH = 20;
     private final int HEIGHT = 20;
+    
     private ImageView[][] array;
+
     private ImageView one;
     private ImageView two;
     private ImageView three;
 
+    private GameGrid grid;
+
     public World(){
+        grid = new GameGrid();
         array = new ImageView[WIDTH][HEIGHT];
+        for(int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (grid.getArrayValue(i, j) == 2) {
+
+                } else if (grid.getArrayValue(i, j) == 1) {
+
+                } else {
+
+                }
+            }
+        }
         initialize();
     }
 
@@ -37,12 +42,6 @@ public class World {
         for(int i = 0; i < array.length; i++){
             for (int j = 0; j < array[i].length; j++){
 
-            }
-        }
-    }
->>>>>>> origin/master
-
-                }
             }
         }
     }
