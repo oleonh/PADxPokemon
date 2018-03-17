54fd013295d5c5166ca7b7f9b232f00f1ef9083e
vgmachinist <37482860+vgmachinist@users.noreply.github.com>
2018-03-17 15:48:19 -0700

    added more to the world class

--- app/src/main/java/com/example/oleonh/padxpokemon/World.java
+++ app/src/main/java/com/example/oleonh/padxpokemon/World.java
@@ -5,6 +5,28 @@ package com.example.oleonh.padxpokemon;
  */
 
 import android.graphics.*;
+import android.media.Image;
+import android.widget.ImageView;
+
 public class World {
+    private final int WIDTH = 20;
+    private final int HEIGHT = 20;
+    private ImageView[][] array;
+    private ImageView one;
+    private ImageView two;
+    private ImageView three;
+
+    public World(){
+        array = new ImageView[WIDTH][HEIGHT];
+        initialize();
+    }
+
+    private void initialize(){
+        for(int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+
+            }
+        }
+    }
 
 }
